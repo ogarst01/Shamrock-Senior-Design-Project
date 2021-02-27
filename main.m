6b2832adefeefaf44d1bba00079f971492e6f9cf
Melissa Rowland <lisrow13@gmail.com>
2021-02-27 12:57:23 -0500

    pseudocode draft of main integration script

---
+++ main.m
@@ -0,0 +1,43 @@
+%{
+Senior Design
+Team Shamrock
+last updated: 2/27/21
+
+main.m
+
+Main script of asteroid landing hazard detection & avoidance simulation.
+
+Questions/Ideas/Future Improvements:
+-possibly make separate functions for the sections if they get too long
+-do we need to have one loop with both KF and image processing/HDA instead
+of separate sections?
+%}
+
+%TODO: Set up params struct
+
+%% Image Processing
+%TODO: read in series of images and perform the following activities in a
+%loop over them
+
+%Read in image
+
+%Perform shadow detection
+
+%Perform computer vision
+
+%Perform TRN
+
+%% Lidar Processing
+
+%% IMU Data Processing
+
+%% Kalman Filter 
+
+%% Hazard Detection and Avoidance
+
+%Combine hazard maps
+
+%% Format outputs
+
+
+
