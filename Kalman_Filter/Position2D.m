2959fbae6a15e93fdf6e41d33b72114de262b6e7
Olive Garst <ogarst@gmail.com>
2021-04-27 20:54:47 -0400

    2d Kalman Filter

--- Kalman_Filter/Position2D.m
+++ Kalman_Filter/Position2D.m
@@ -5,33 +5,29 @@ classdef Position2D
         delT         % Time step
         sigp         % Position measurement noise
         siga         % Acceleration measurement noise (m/s^2)
+        sig_TRN      % TRN noise
     end
     
     methods
-        function [A,B,H,P,Q,R] = CreateFiltObj(obj)
-            A = [1, 0,      -obj.delT^2/2, 0, 0,            0;
-                 0, 1,          -obj.delT, 0, 0,            0;
-                 0, 0,                  1, 0, 0,            0;
-                 0, 0,                  0, 1, 0,-obj.delT^2/2;
-                 0, 0,                  0, 0, 1,    -obj.delT;
-                 0, 0,                  0, 0, 0,            1];
-            B = [obj.delT^2/2; 
-                 obj.delT; 
-                 0;
-                 obj.delT^2/2; 
-                 obj.delT; 
-                 0];
-            H = [1, 0, 0, 0, 0, 0;
-                 0, 0, 0, 1, 0, 0];
-            Q = [(obj.delT^4/4)*obj.sigp^2, (obj.delT^3/2)*obj.sigp^2,   -obj.delT^2/2,                         0,                         0,             0;
-                 (obj.delT^3/2)*obj.sigp^2,     obj.delT^2*obj.sigp^2,               0,                         0,                         0,             0;
-                                         0,                         0,               1,                         0,                         0,             0;
-                                         0,                         0,               0, (obj.delT^4/4)*obj.sigp^2, (obj.delT^3/2)*obj.sigp^2, -obj.delT^2/2;
-                                         0,                         0,               0, (obj.delT^3/2)*obj.sigp^2,     obj.delT^2*obj.sigp^2,             0;
-                                         0,                         0,               0,                         0,                         0,             1];
-            R = obj.siga^2*eye(2);
-            P = 1e-10*eye(6);
+        function [A,B,H,P,Q,R,W] = CreateFiltObj(obj)
+            A = [0, obj.delT, 0,          0;
+                 0,        1, 0,          0;
+                 0,        0, 1,   obj.delT;
+                 0,        0, 0,         1];
+            B = [obj.delT^2/2, 0;
+                 obj.delT,     0;
+                 0,            obj.delT^2/2; 
+                 0,            obj.delT];
+            H = [1, 0, 0, 0;
+                 0, 0, 1, 0];
+            Q = [(obj.delT^4/4)*obj.sigp^2, (obj.delT^3/2)*obj.sigp^2,                         0,                         0;
+                 (obj.delT^3/2)*obj.sigp^2,     obj.delT^2*obj.sigp^2,                         0,                         0;
+                                         0,                         0, (obj.delT^4/4)*obj.sigp^2, (obj.delT^3/2)*obj.sigp^2;
+                                         0,                         0, (obj.delT^3/2)*obj.sigp^2,     obj.delT^2*obj.sigp^2];
+            
+            R = obj.sig_TRN^2.*eye(2);
+            P = 1e-10*eye(4);
+            W = (obj.siga * obj.delT)^2;
         end
     end
 end
-
